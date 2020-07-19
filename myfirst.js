ddec34e039e35646c8a912172cd53248003d154e
anirudh <anirudh@gmail.com>
2020-07-19 12:09:45 +0530

    again

--- myfirst.js
+++ myfirst.js
@@ -1,6 +1,6 @@
-var http = require('http');
+import { createServer } from 'http';
 
-http.createServer(function (req, res) {
+createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello World!');
 }).listen(8080);

f23433269a978ed328f95cc5351ab8a50670f149
anirudh <anirudh@gmail.com>
2020-07-20 01:05:02 +0530

    require2222

--- abc.js
+++ abc.js
@@ -1,12 +1,5 @@
 var http = require('http');
-var dt = require('./abc');
-
-exports.myDateTime = function ()
- {
-    return Date();
-  };
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("The date and time are currently: " + dt.myDateTime());
-  res.end();
-}).listen(8080);
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Hello World!');
+  }).listen(8080);

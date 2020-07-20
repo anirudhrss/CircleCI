6cd977b3c133bf2cbbecca202cb4c2b836c0f904
anirudh <anirudh@gmail.com>
2020-07-20 14:15:04 +0530

    demo32

--- demo_module.js
+++ demo_module.js
@@ -1,7 +1,6 @@
 var http = require('http');
-exports.myDateTime = function () {
-    return Date();
-  };
+var dt = require('./myfirstmodule');
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write("The date and time are currently: " + dt.myDateTime());

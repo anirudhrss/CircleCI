08b8098961368bac20f0e576e312fce0eae30fa9
anirudh <anirudh@gmail.com>
2020-08-08 17:06:27 +0530

    travis

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice' },
+  { id: 12, name: 'Ramesh' },
+  { id: 13, name: 'Somani' },
+  { id: 14, name: 'Bhandari' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'AniBoss' },
+  { id: 69, name: 'SirAnirudh' }
+];
